78ac9dafddf79905bcc7f09fb2c811431278f8e9
mc-b <marcel.bernet@ch-open.ch>
2021-05-13 13:45:41 +0200

    Umstellung auf registry.gitlab.com

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 #	Installationsscript iot.kafka
 
 # Abhaengige Images
-docker pull confluentinc/cp-kafka:latest
-docker pull confluentinc/cp-zookeeper:latest
-docker pull devicexx/mqtt-kafka-bridge 
+docker pull registry.gitlab.com/mc-b/iot-kafka/cp-kafka:latest
+docker pull registry.gitlab.com/mc-b/iot-kafka/cp-zookeeper:latest
+#docker pull devicexx/mqtt-kafka-bridge 
 
